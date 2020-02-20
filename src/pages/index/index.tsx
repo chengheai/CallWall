fca34ab93ad4b4aab7abf7df62c8dab0d72a41e0
chengheai <842385139@qq.com>
2020-02-20 22:40:59 +0800

    for a stage

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,18 +1,17 @@
-import Taro, { Component, Config } from '@tarojs/taro'
-import { View, Text } from '@tarojs/components'
-import './index.less'
+import Taro, { Component, Config } from '@tarojs/taro';
+import { View, Text } from '@tarojs/components';
+import './index.less';
 
 export default class Index extends Component {
+  componentWillMount() {}
 
-  componentWillMount () { }
+  componentDidMount() {}
 
-  componentDidMount () { }
+  componentWillUnmount() {}
 
-  componentWillUnmount () { }
+  componentDidShow() {}
 
-  componentDidShow () { }
-
-  componentDidHide () { }
+  componentDidHide() {}
 
   /**
    * 指定config的类型声明为: Taro.Config
@@ -22,14 +21,14 @@ export default class Index extends Component {
    * 提示和声明 navigationBarTextStyle: 'black' | 'white' 类型冲突, 需要显示声明类型
    */
   config: Config = {
-    navigationBarTitleText: '首页'
-  }
+    navigationBarTitleText: '首页',
+  };
 
-  render () {
+  render() {
     return (
-      <View className='index'>
-        <Text>Hello world!</Text>
+      <View className="c-wrapper">
+        <Text className="t-scroll-text">Hello world!</Text>
       </View>
-    )
+    );
   }
 }
