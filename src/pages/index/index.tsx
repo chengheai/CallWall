d23eac35a9a5b2b1969c5db2bf41444eaffb01ce
chengheai <842385139@qq.com>
2020-02-21 10:17:22 +0800

    taro for a stage

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -3,7 +3,100 @@ import { View, Text } from '@tarojs/components';
 import './index.less';
 
 export default class Index extends Component {
-  componentWillMount() {}
+  constructor(props) {
+    super(props);
+    this.state = {
+      displayText: '手持弹幕',
+      panelFlag: false,
+      barFlag: true,
+      colorIndex: 0,
+      speedIndex: 0,
+      sizeIndex: 0,
+      scorllDuration: 5000,
+      hintText: '(ﾉ◕ヮ◕)ﾉ点击非输入区域即可隐藏/显示界面哦！',
+      textLen: 0,
+      animation: {},
+      currentSpeed: 0,
+      sizeArr: [
+        {
+          name: '正常',
+          value: 40,
+          active: true,
+        },
+        {
+          name: '小',
+          value: 25,
+          active: false,
+        },
+        {
+          name: '大',
+          value: 60,
+          active: false,
+        },
+        {
+          name: '超大',
+          value: 80,
+          active: false,
+        },
+      ],
+      speedArr: [
+        {
+          name: '正常',
+          value: 5000,
+          active: true,
+        },
+        {
+          name: '慢',
+          value: 7000,
+          active: false,
+        },
+        {
+          name: '快',
+          value: 3000,
+          active: false,
+        },
+      ],
+      colorArr: [
+        {
+          value: '#fff',
+          active: true,
+        },
+        {
+          value: '#f00',
+          active: false,
+        },
+        {
+          value: '#DA70D6',
+          active: false,
+        },
+        {
+          value: '#FFFFE0',
+          active: false,
+        },
+        {
+          value: '#00FFFF',
+          active: false,
+        },
+        {
+          value: '#1E90FF',
+          active: false,
+        },
+        {
+          value: '#F0FFFF',
+          active: false,
+        },
+        {
+          value: '#00FF00',
+          active: false,
+        },
+      ],
+    };
+  }
+  componentWillMount() {
+    this.setState({
+      windowHeight: window.document.height,
+    });
+  }
 
   componentDidMount() {}
 
